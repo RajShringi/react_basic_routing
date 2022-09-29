dea7f1b7b419d605a85440c8d0e1830a4e04d3dd
raj shringi <raj24shringi@gmail.com>
2022-09-29 23:39:03 +0530

    move BrowserRouter to index and Header component to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Main from "./Main";
+import Header from "./Header";
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <Main />
       </div>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Articles from "./Articles";
 import Books from "./Books";
 import Help from "./Help";
@@ -6,38 +6,34 @@ import People from "./People";
 import Sidebar from "./Sidebar";
 import Home from "./Home";
 import SingleArticle from "./SingleArticle";
-import Header from "./Header";
 import NotFound from "./NotFound";
 
 function Main() {
   return (
-    <BrowserRouter>
-      <Header />
-      <div className="flex justify-between">
-        <Sidebar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/books">
-            <Books />
-          </Route>
-          <Route exact path="/articles">
-            <Articles />
-          </Route>
-          <Route path="/people">
-            <People />
-          </Route>
-          <Route path="/help">
-            <Help />
-          </Route>
-          <Route path="/articles/:slug" component={SingleArticle} />
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className="flex justify-between">
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/books">
+          <Books />
+        </Route>
+        <Route exact path="/articles">
+          <Articles />
+        </Route>
+        <Route path="/people">
+          <People />
+        </Route>
+        <Route path="/help">
+          <Help />
+        </Route>
+        <Route path="/articles/:slug" component={SingleArticle} />
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 export default Main;
